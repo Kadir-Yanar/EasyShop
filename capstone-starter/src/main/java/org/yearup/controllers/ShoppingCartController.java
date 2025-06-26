3d58a746b8ab137a2b26ea1b3900dd161ceb3990
Abdulkadir Yanar <abdulkadiryanar03@gmail.com>
2025-06-26 16:22:52 -0700

    Fixed get cart methods and added addToProduct Method

--- capstone-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -3,10 +3,7 @@ package org.yearup.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
@@ -50,7 +47,7 @@ public class ShoppingCartController
             int userId = user.getId();
 
             // use the shoppingcartDao to get all items in the cart and return the cart
-            return null;
+            return shoppingCartDao.getByUserId(userId);
         }
         catch(Exception e)
         {
@@ -60,6 +57,19 @@ public class ShoppingCartController
 
     // add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be added
+    @PostMapping("products/{productId}")
+    public void addProductToCart(@PathVariable int productId,Principal principal){
+        try {
+            String userName = principal.getName();
+            User user = userDao.getByUserName(userName);
+            int userId = user.getId();
+
+            shoppingCartDao.addProductToCart(userId,productId);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,"Oops..system error");
+        }
+    }
+
 
 
     // add a PUT method to update an existing product in the cart - the url should be
