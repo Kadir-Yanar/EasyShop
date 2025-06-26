f1b7d57e7a958b70954abb96433a39281d18954a
Abdulkadir Yanar <abdulkadiryanar03@gmail.com>
2025-06-25 22:51:13 -0700

    Fixed Bugs

--- capstone-starter/src/main/java/org/yearup/data/mysql/MySqlProductDao.java
+++ capstone-starter/src/main/java/org/yearup/data/mysql/MySqlProductDao.java
@@ -22,9 +22,10 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao
     public List<Product> search(Integer categoryId, BigDecimal minPrice, BigDecimal maxPrice, String color)
     {
         List<Product> products = new ArrayList<>();
-
+        // Fixed minPrice operator and Added max price condition
         String sql = "SELECT * FROM products " +
                 "WHERE (category_id = ? OR ? = -1) " +
+                "   AND (price >= ? OR ? = -1) " +
                 "   AND (price <= ? OR ? = -1) " +
                 "   AND (color = ? OR ? = '') ";
 
@@ -35,13 +36,16 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao
 
         try (Connection connection = getConnection())
         {
+            //Connected missing max price and Updated Color indexs
             PreparedStatement statement = connection.prepareStatement(sql);
             statement.setInt(1, categoryId);
             statement.setInt(2, categoryId);
             statement.setBigDecimal(3, minPrice);
             statement.setBigDecimal(4, minPrice);
-            statement.setString(5, color);
-            statement.setString(6, color);
+            statement.setBigDecimal(5, maxPrice);
+            statement.setBigDecimal(6, maxPrice);
+            statement.setString(7, color);
+            statement.setString(8, color);
 
             ResultSet row = statement.executeQuery();
 
