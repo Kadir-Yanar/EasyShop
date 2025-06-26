e5a032b61eec80f79636f1ff9870e49cc44736e4
Abdulkadir Yanar <abdulkadiryanar03@gmail.com>
2025-06-26 15:36:34 -0700

    Added constructor and get mapping annotation

--- capstone-starter/src/main/java/org/yearup/data/ShoppingCartDao.java
+++ capstone-starter/src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,9 +1,17 @@
 package org.yearup.data;
 
+import org.yearup.models.Category;
 import org.yearup.models.ShoppingCart;
+
+import java.util.List;
 
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
-    // add additional method signatures here
+    void addProductToCart(int userId, int productId);
+    void updateProductQuantity(int userId, int productId, int quantity);
+    void removeProductFromCart(int userId, int productId);
+    void clearCart(int userId);
+    void update(int categoryId, Category category);
+    void delete(int categoryId);
 }
